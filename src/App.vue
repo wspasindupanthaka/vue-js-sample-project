30392d234d0c4b1354e1c20acf69b014bcfbf455
pasindu <pasindu@codegen.net>
2019-10-18 20:07:39 +0530

    creating question box

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+     <Header />
+     <b-container class="bv-example-row">
+        <b-row>
+           <b-col sm="6" offset="3">
+              <QuestionBox />
+           </b-col>
+        </b-row>
+      </b-container>    
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    QuestionBox
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
