021a6341e0089503fa9c3fdb436c851e0c5f100d
pasindu <pasindu@codegen.net>
2019-10-25 10:23:00 +0530

    finishing the app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
   <div id="app">
-     <Header />
-     <b-container class="bv-example-row">
-        <b-row>
-           <b-col sm="6" offset="3">
-              <QuestionBox />
-           </b-col>
-        </b-row>
-      </b-container>    
+    <Header 
+    :numCorrect="numCorrect"
+    :numTotal="numTotal"
+    />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            v-if="questions.length>0 "
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import QuestionBox from './components/QuestionBox.vue'
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Header,
     QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    };
+  },
+  methods: {
+    next() {
+      this.index++;
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        ++this.numCorrect;
+      }
+      ++this.numTotal;
+    }
+  },
+  mounted: function() {
+    fetch(
+      "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple",
+      {
+        method: "get"
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.questions = jsonData.results;
+      });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,18 +1,99 @@
 <template>
-    <div class="question-box-container">
-        <b-jumbotron>
-             <template v-slot:lead>
-               Some question here ? 
-            </template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>{{currentQuestion.question}}</template>
 
-            <hr class="my-4">
+      <hr class="my-4" />
 
-            <p>
-               List of answers
-            </p>
+      <b-list-group>
+        <b-list-group-item
+          v-for="answer in pushAnswers"
+          :key="answer"
+          @click="selectAnswer(answer)"
+          :class="[!answered && 
+          !answered && selectedAnswer===answer ? 'selected' : 
+          answered && currentQuestion.correct_answer===answer ? 'correct' : 
+          answered && selectedAnswer===answer && currentQuestion.correct_answer!==answer ? 'incorrect' :'']"
+        >{{answer}}</b-list-group-item>
+      </b-list-group>
 
-            <b-button variant="primary" href="#">Submit</b-button>
-            <b-button variant="success" href="#">Navigate</b-button>
-        </b-jumbotron>
-    </div>
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedAnswer==='' || answered"
+      >Submit</b-button>
+      <b-button variant="success" href="#" @click="next">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+    increment: Function
+  },
+  data() {
+    return {
+      selectedAnswer: "",
+       correctIndex:90,
+      answered:false
+    }; 
+  },
+  watch : {
+      currentQuestion : {
+          handler() {
+              this.selectedAnswer="",
+              this.answered=false
+          }
+      }
+  },
+  methods: {
+    selectAnswer(answer) {
+      console.log(answer);
+      this.selectedAnswer = answer;
+    },
+    submitAnswer() {  
+        
+        let correct=false
+
+        if(this.selectedAnswer===this.currentQuestion.correct_answer)
+            correct=true;
+        
+        this.answered=true
+        this.increment(correct) 
+    }
+  },
+  computed: {
+    pushAnswers() {
+      let answers = this.currentQuestion.incorrect_answers;
+      answers.push(this.currentQuestion.correct_answer);   
+     
+      return answers; 
+    }
+  }
+};
+</script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.list-group-item:hover {
+  background: #eeeeee;
+  cursor: pointer;
+}
+.btn {
+  margin: 0 5px;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: lightcoral;
+}
+</style>
